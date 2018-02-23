1c14bf99db132a672949eb6a9108df2b6cf7dd87
Daniel Doyle <dbdoyle182@gmail.com>
2018-02-22 21:52:52 -0500

    pseudocoding

---
+++ javascript/game.js
@@ -0,0 +1,21 @@
+// Pick a random word
+
+
+
+// While the word has not been guessed {
+    // Show the player their current progress
+    // Get a guess from the player
+
+
+// If the player wnats to quit the game {
+    // Quit the game
+// }
+// Else if the guess is not a single letter {
+    // Tell the player to pick a single letter
+// }
+// Else {
+    // IF the guess is in the word {
+        // Update the player's progress with the guess
+        // }
+    // } 
+// }
